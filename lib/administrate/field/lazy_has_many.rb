fffc8f572ac72d3a0f25b2002e5312396f366420
Harley Trung <harley@coderpush.com>
2020-11-10 10:17:59 +0700

    add basic files

--- lib/administrate/field/lazy_has_many.rb
+++ lib/administrate/field/lazy_has_many.rb
@@ -2,9 +2,8 @@ require "administrate/field/lazy_has_many/version"
 
 module Administrate
   module Field
-    module LazyHasMany
+    module LazyHasMany < Administrate::Field::HasMany
       class Error < StandardError; end
-      # Your code goes here...
     end
   end
 end
